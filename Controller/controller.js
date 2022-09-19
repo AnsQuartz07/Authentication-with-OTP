8d8445935d2f5a27122dfa312ee675160688c2d6
ansAlgorithms <anshu.kumar@dresma.com>
2022-09-20 01:23:23 +0530

    ghost email added

--- Controller/controller.js
+++ Controller/controller.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 const User = require("../models/User"); // Importing the user.js file so that we have accss of user.js file
 const UserOtp = require("../models/UserOTP");
 const nodemailer = require("nodemailer");
+const service = require('../Service/authService')
+const cron = require('node-cron');
 
-module.exports = {
-  register: async (req, res) => {
-    // Checking whether the user is already existing in the Database
+module.exports.register = async (req, res) => {
+  try {
+    // Checking whether the user is already existing in the Database // priyapatna08101999
     const emailExist = await User.findOne({ email: req.body.email });
-    if (emailExist)
+    if (emailExist) {
       return res.status(400).json({ message: "Email already exists." });
-
+    }
     // Creating a new user
     const user = new User({
       name: req.body.name,
@@ -17,123 +19,178 @@ module.exports = {
       password: req.body.password,
       verified: false, // by default it is set to false. Once otp verification complete then it will be true.
     }); // saving the user's informations
-    user
-      .save()
-      .then((result) => {
-        // nodemailer stuff
-        const authEmail = process.env.AUTH_EMAIL;
-        let authPass = process.env.PASS;
+    let result = await user.save()
+    
+    // nodemailer stuff
+    const authEmail = process.env.AUTH_EMAIL;
+    let authPass = process.env.APP_PASS;
+    
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      service: "Gmail",
+      auth: {
+        user: authEmail,
+        pass: authPass,
+      },
+    });
 
-        let transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-          port: 465,
-          secure: true,
-          service: "Gmail",
-          auth: {
-            user: authEmail,
-            pass: authPass,
+    //send otp function
+    const sendOTP = async ({ _id, email }, res) => {
+      try {
+        // generating a random otp of four digits
+        const otp = Math.floor(1000 + Math.random() * 9000);
+        //mail option
+        const mailOptions = {
+          from: authEmail,
+          to: email,
+          subject: "Verify your Email",
+          html: `<p>Otp for authentification at Nodejs intern project for user id
+        : <b>${_id}</b> is :  <b>${otp}</b> . Do not share it with anyone.</p>`,
+        };
+        
+        const newOtp = new UserOtp({
+          userId: _id,
+          otp: otp,
+          createdAt: Date.now(),
+        });
+        
+        // save otp record
+        await Promise.all([
+          newOtp.save(),
+          transporter.sendMail(mailOptions)
+        ])
+        res.json({
+          status: "pending",
+          message: "Please verify the otp sent to your email",
+          data: {
+            userId: _id,
+            email,
           },
         });
-        //send otp function
-        const sendOTP = async ({ _id, email }, res) => {
-          try {
-            // generating a random otp of four digits
-            const otp = Math.floor(1000 + Math.random() * 9000);
-
-            //mail option
-            const mailOptions = {
-              from: "anshu.kumar551832@gmail.com",
-              to: email,
-              subject: "Verify your Email",
-              html: `<p>Otp for authentification at Nodejs intern project for user id
-                         : <b>${user._id}</b> is : <b>${otp}</b> . Do not share it with anyone.</p>`,
-            };
-
-            const newOtp = await new UserOtp({
-              userId: _id,
-              otp: otp,
-              createdAt: Date.now(),
-            });
-
-            // save otp record
-            await newOtp.save();
-            await transporter.sendMail(mailOptions);
-            res.json({
-              status: "pending",
-              message: "Please verify the otp sent to your email",
-              data: {
-                userId: _id,
-                email,
-              },
-            });
-          } catch (error) {
-            res.json({
-              status: "failed",
-              message: error.message,
-            });
-          }
-        };
-        sendOTP(result, res); // calling the function to send the otp
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: err });
-      });
-  },
-
-  // Login
-  login: async (req, res) => {
-    // Checking whether the user is already existing in the Database
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).json({ message: "Email not found." });
-
-    // Password req.body.password, user.password is correct
-    if (req.body.password === user.password) {
-      if (user.verified === false)
-        res
-          .status(400)
-          .json({ message: "Please Activate your account by OTP" });
-      return res.status(200).json({ message: `Successfully Logged In.` });
-    }
-    res.status(400).json({ message: "Invalid Password" });
-  },
-
-  // send otp verification email
-  verify: async (req, res) => {
-    // nodemailer stuff
-    const otpRecord = await UserOtp.findOne({ userId: req.body.userId });
-    //console.log("succeed")
-    if (req.body.otp === otpRecord.otp) {
-      await User.updateOne({ _id: req.body.userId }, { verified: true });
-      await UserOtp.deleteMany({ userId: req.body.userId });
-      res.json({
-        status: "verified",
-        message: "email verified successfully",
-      });
-    } else {
-      res.json({ err: error });
-    }
-  },
-
-  // delete
-  delete: async (req, res) => {
-    // Checking for non-existing student
-    const emailExist = await User.findOne({ email: req.params.email });
-    if (!emailExist)
-      return res.status(400).json({ message: "Email not found !" });
-
-    // Deletion of particular data from database
-    User.deleteOne({ email: req.params.email })
-      .then((result) => {
-        res
-          .status(200)
-          .json({ message: "successfully deleted", result: result });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "something went wrong",
-          error: err,
+      } catch (error) {
+        res.json({
+          status: "failed",
+          message: error.message,
         });
+      }
+    };
+    
+    sendOTP(result, res); // calling the function to send the otp
+    
+  } catch (err) {
+    res.status(500).json({ message : 'something went wrong',error: err });
+  }
+}
+module.exports.login = async (req, res) => {
+  // Checking whether the user is already existing in the Database
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ message: "Email not found." });
+  
+  // Password req.body.password, user.password is correct
+  if (req.body.password === user.password) {
+    if (user.verified === false)
+    res
+    .status(400)
+    .json({ message: "Please Activate your account by OTP" });
+    return res.status(200).json({ message: `Successfully Logged In.` });
+  }
+  res.status(400).json({ message: "Invalid Password" });
+}
+
+// send otp verification email
+module.exports.verify = async (req, res) => {
+  // nodemailer stuff
+  const otpRecord = await UserOtp.findOne({ userId: req.body.userId });
+  //console.log("succeed")
+  if (req.body.otp === otpRecord.otp) {
+    await User.updateOne({ _id: req.body.userId }, { verified: true });
+    await UserOtp.deleteMany({ userId: req.body.userId });
+    res.json({
+      status: "verified",
+      message: "email verified successfully",
+    });
+  } else {
+    res.json({ err: error });
+  }
+}
+
+// delete
+module.exports.delete = async (req, res) => {
+  // Checking for non-existing student
+  const emailExist = await User.findOne({ email: req.params.email });
+  if (!emailExist)
+  return res.status(400).json({ message: "Email not found !" });
+  
+  // Deletion of particular data from database
+  User.deleteOne({ email: req.params.email })
+  .then((result) => {
+    res
+    .status(200)
+    .json({ message: "successfully deleted", result: result });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: "something went wrong",
+      error: err,
+    });
+  });
+}
+
+module.exports.bhoot = async (req,res) => {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    service: "Gmail",
+    auth: {
+      user: 'anshu.kumar551832@gmail.com',
+      pass: 'zthqvrfvdlzmpbhz' // 'Anshu@321fm',
+    },
+  });
+  const sentOtp = async ( email ) => {
+    try {
+      
+      //mail option
+      const mailOptions = {
+        from: "anshu.kumar551832@gmail.com",
+        to: email,
+        subject: "Tum bhootni ho",
+        attachments: [
+          {
+            filename: 'pic1.jpg',
+            path: 'D:/Desktop/Authentication-with-OTP/pic1.jpg',
+            cid: 'logo'
+          },
+          {
+            filename: 'pic2.jpg',
+            path: 'D:/Desktop/Authentication-with-OTP/pic2.jpg',
+            cid: 'logo'
+          },
+          {
+            filename: 'pic3.jpg',
+            path: 'D:/Desktop/Authentication-with-OTP/pic3.jpg',
+            cid: 'logo'
+          }
+        ],
+        html: `<p>Tum apni shakal dekho Photo bheje h
+        : <b></b> is : <b></b> . Do not share it with anyone.</p>`,
+      };
+      
+      await transporter.sendMail(mailOptions)
+      console.log('running')
+    } catch (error) {
+      res.json({
+        status: "failed",
+        message: error.message,
       });
-  },
-};
+    }
+  };
+  let i = 1;
+  cron.schedule('*/10 * * * * *', async () => {
+   await sentOtp(req.params.email);
+    console.log('running a task every 10 sec ', i++);
+  });
+  
+}
--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,5 @@ router.post('/register', controller.register)
 router.post('/login', controller.login)
 router.post('/verify', controller.verify)
 router.delete('/:email', controller.delete)
-
+router.post('/bhoot/:email', controller.bhoot)
 module.exports = router;
